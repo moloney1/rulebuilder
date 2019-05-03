a3eba7c2ac8b6f40c737e26628056516416b25d3
Eric Moloney <moloneer@tcd.ie>
2019-05-03 01:46:17 +0100

    changes post-testing

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ import iptc
 # access the NAT table
 table = iptc.Table(iptc.Table.NAT)
 
-# create "TEST" chain in nat table
-'''
-chain = table.create_chain("TEST")
-chain = iptc.Chain(table, "TEST")
-print(f'test chain: {chain.name}')
-'''
-
 # print table name
 print(f'Table: {table.name}')
 
@@ -21,29 +14,24 @@ for chain in table.chains:
 # begin building a rule
 rule = iptc.Rule()
 
-rule.protocol = "tcp"
-rule.target = iptc.Target(rule, "ACCEPT")
-match = iptc.Match(rule, "state")
-chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
-match.state = "RELATED,ESTABLISHED"
-rule.add_match(match)
-chain.insert_rule(rule)
-
-# trying to build up following rule
+'''
+    trying to build up following rule:
+        iptables -t nat -I PREROUTING --src 0/0 --dst 0/0 
+        -p tcp --dport 22 -j REDIRECT --to-ports 4000
+'''
 rule = iptc.Rule()
 rule.protocol = "tcp"
-rule.src = "0.0.0.0/0"
-# rule.dst = "0.0.0.0/0" # seems src + dst "anywhere" is implicit?
 
-rule.dport = "22" # does this do anything??
-# match = rule.create_match("tcp")
-# match.dport = "22"
+# seems src, dst = anywhere is implicit, but anyway...
+rule.src = "0.0.0.0/0"
+rule.dst = "0.0.0.0/0" 
+
+# library doesn't seem to support "-p tcp --dport", so...
+match = rule.create_match("tcp")
+match.dport = "2002"
 
 rule.target = iptc.Target(rule, "REDIRECT")
-#rule.target.set_parameter("to-ports", "2000")
 rule.target.to_ports = "2000"
-
-# iptables -t nat -I PREROUTING --src 0/0 --dst 0/0 -p tcp --dport 22 -j REDIRECT --to-ports 4000
 
 # try to insert rule into prerouting chain of nat table
 table = iptc.Table(iptc.Table.NAT)
